f07b2c6f1b83416cd2c1016efab4014dc838c6d7
An <andoycle@gmail.com>
2021-11-12 07:22:55 +0700

    FirstCode

---
+++ Legiaan baitap/Legiaan baitap/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace baitapcuatui
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("***Moi nhap ung vien so 1");
+            Console.Write("+Nhap ho ten:");
+            string tenuv = Console.ReadLine();
+            Console.Write("+Nhap nam sinh:");
+            string namsinh = Console.ReadLine();
+            Console.Write("+Nhap gioi tinh:");
+            string gioitinh = Console.ReadLine();
+            Console.Write("+Nhap chieu cao:");
+            string chieucao = Console.ReadLine();
+            Console.Write("+Nhap can nang:");
+            string cannang = Console.ReadLine();
+            Console.Write("+Nhap que quan:");
+            string quequan = Console.ReadLine();
+            Console.Write("+Nhap chuyen nganh:");
+            string chuyennganh = Console.ReadLine();
+            Console.Write("+Nhap diem TB:");
+            string diemtb = Console.ReadLine();
+
+            Console.WriteLine("***Moi nhap ung vien so 2");
+            Console.Write("+Nhap ho ten:");
+            string tenuv2 = Console.ReadLine();
+            Console.Write("+Nhap nam sinh:");
+            string namsinh2 = Console.ReadLine();
+            Console.Write("+Nhap gioi tinh:");
+            string gioitinh2 = Console.ReadLine();
+            Console.Write("+Nhap chieu cao:");
+            string chieucao2 = Console.ReadLine();
+            Console.Write("+Nhap can nang:");
+            string cannang2 = Console.ReadLine();
+            Console.Write("+Nhap que quan:");
+            string quequan2 = Console.ReadLine();
+            Console.Write("+Nhap chuyen nganh:");
+            string chuyennganh2 = Console.ReadLine();
+            Console.Write("+Nhap diem TB:");
+            string diemtb2 = Console.ReadLine();
+
+            Console.WriteLine("***Moi nhap ung vien so 3");
+            Console.Write("+Nhap ho ten:");
+            string tenuv3 = Console.ReadLine();
+            Console.Write("+Nhap nam sinh:");
+            string namsinh3 = Console.ReadLine();
+            Console.Write("+Nhap gioi tinh:");
+            string gioitinh3 = Console.ReadLine();
+            Console.Write("+Nhap chieu cao:");
+            string chieucao3 = Console.ReadLine();
+            Console.Write("+Nhap can nang:");
+            string cannang3 = Console.ReadLine();
+            Console.Write("+Nhap que quan:");
+            string quequan3 = Console.ReadLine();
+            Console.Write("+Nhap chuyen nganh:");
+            string chuyennganh3 = Console.ReadLine();
+            Console.Write("+Nhap diem TB:");
+            string diemtb3 = Console.ReadLine();
+
+        }
+
+    }
+}
